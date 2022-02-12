ccf662185f18986645ae2edbafe448c271656ef4
eijjeka <eijjeka@gmail.com>
2022-02-12 22:03:58 +0200

    update files

--- src/index.js
+++ src/index.js
@@ -1,1 +1,51 @@
 import './sass/main.scss';
+import { getPopularMovies } from './js/api/movieApi';
+import moviesListTemplate from './js/components/moviesList.hbs';
+import { generateImgPath } from './js/utils';
+import { Pagination } from './js/pagination';
+
+const moviesListRef = document.querySelector('.movies-wrapper');
+const nextPageRef = document.querySelector('.next-page');
+const prevPageRef = document.querySelector('.prev-page');
+const currentPageRef = document.querySelector('.current-page');
+
+const moviePagination = new Pagination({
+  total: 100,
+  onChange(value) {
+    handlePageChange(value);
+    console.log(value);
+    currentPageRef.textContent = value;
+  },
+});
+
+const renderMovieList = movies => {
+  const moviesList = movies.map(movie => {
+    const { original_title, poster_path } = movie;
+
+    return {
+      original_title,
+      poster: generateImgPath(poster_path),
+    };
+  });
+
+  moviesListRef.innerHTML = moviesListTemplate(moviesList);
+};
+
+const handlePageChange = currentPage => {
+  getPopularMovies(currentPage).then(({ data }) => {
+    renderMovieList(data.results);
+  });
+};
+
+nextPageRef.addEventListener('click', () => {
+  moviePagination.nextPage();
+});
+
+prevPageRef.addEventListener('click', () => {
+  moviePagination.prevPage();
+});
+
+getPopularMovies().then(({ data }) => {
+  const { results: movies } = data;
+  renderMovieList(movies);
+});
---
+++ src/js/api/settings.js
@@ -0,0 +1,6 @@
+const settings = {
+    movieKey: 'cd00d8c55831cbeec8ccdad1db16d6d2',
+    movieApiUrl: 'https://api.themoviedb.org/3'
+}
+
+export default settings
